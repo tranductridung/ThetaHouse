1f779ac7f3aaeb7c4bd44413225b30795610ea4b
Tri Dung <tranductridung0103@gmail.com>
2025-08-30 22:29:03 +0700

    fix origin in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://thetahouse.vercel.app'],
     credentials: true,
   });
 
